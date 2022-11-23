165e03d13f0a6818c02e801a6812f0e9cd3b3ac4
MSI\h_mat <h_matsumoto@hotmail.com>
2022-11-23 22:59:11 +0900

    17,2,6

--- abc276/e.py
+++ abc276/e.py
@@ -16,79 +16,30 @@ def search(current, goal, cmap, path):
     H, W = len(cmap), len(cmap[0])
     h, w = current
     start = goal
-    # print("IN:", list(map(lambda t:(t[0]+1, t[1]+1), [current])))
     reached = False
     path.append(current)
     if 4 <= len(path) and current == goal:
-        print(goal)
-        print(path)
         reached = True
-        # return reached
-    print(current)
-    # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-    if not reached and h+1 < H and cmap[h+1][w] != '#' and (h+1, w) not in path:
-        # print("RIGHT:", list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print("RIGHT:", current, " --> ", (h+1, w), end=" || ")
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print(path)
-        # print((h+1, w), cmap[h+1][w])
+    if not reached and h+1 < H and cmap[h+1][w] != '#' and ((4 <= len(path) and (h+1, w) == start) or ((h+1, w) != start and (h+1, w) not in path)):
         reached = search((h+1, w), goal, cmap, path)
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
         if len(path) and not reached:
             path.pop()
-    if not reached and 0 <= h-1 and cmap[h-1][w] != '#' and (h-1, w) not in path:
-        # print("LEFT:", list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print("LEFT:", current, " --> ", (h-1, w), end=" || ")
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print(path)
-        # print((h-1, w), cmap[h-1][w])
+    if not reached and 0 <= h-1 and cmap[h-1][w] != '#' and ((4 <= len(path) and (h-1, w) == start) or ((h-1, w) != start and (h-1, w) not in path)):
         reached = search((h-1, w), goal, cmap, path)
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
         if len(path) and not reached:
             path.pop()
-    if not reached and w+1 < W and cmap[h][w+1] != '#' and (h, w+1) not in path:
-        # print("DOWN:", list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print("DOWN:", current, " --> ", (h, w+1), end=" || ")
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print(path)
-        # print((h, w+1), cmap[h][w+1])
+    if not reached and w+1 < W and cmap[h][w+1] != '#' and ((4 <= len(path) and (h, w+1) == start) or ((h, w+1) != start and (h, w+1) not in path)):
         reached = search((h, w+1), goal, cmap, path)
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
         if len(path) and not reached:
             path.pop()
-    # (h, w-1) == start(h, w-1) == start or ((h, w-1) != start and (h, w-1) not in path)
-    print(current, "UP cond: ", not reached, 0 <= w-1,  cmap[h][w-1] != '#', (h, w-1) == start or ((h, w-1) != start and (h, w-1) not in path))
-    if not reached and 0 <= w-1 and cmap[h][w-1] != '#' and ((h, w-1) == start or ((h, w-1) != start and (h, w-1) not in path)):
-        # print("UP:", list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print("UP:", current, " --> ", (h, w-1), end=" || ")
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-        print(path)
-        # print((h, w-1), cmap[h][w-1])
+    if not reached and 0 <= w-1 and cmap[h][w-1] != '#' and ((4 <= len(path) and (h, w-1) == start) or ((h, w-1) != start and (h, w-1) not in path)):
         reached = search((h, w-1), goal, cmap, path)
-        # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
         if len(path) and not reached:
             path.pop()
-    # print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-    """
-    if not reached:
-        # print(path)
-        path.pop()
-    """
-    # print("OUT:", list(map(lambda t:(t[0]+1, t[1]+1), [current])))
-    # print("OUT:", current)
     return reached
 
-        
-
 path = []
-# print("start:", start)
-print(search(start, start, cmap=C, path=path))
-print(list(map(lambda t:(t[0]+1, t[1]+1), path)))
-quit()
-goaled = False
-path = [start]
-while not goaled:
-    break
-    
-print(C)
-
+if search(start, start, cmap=C, path=path):
+    print("Yes")
+else:
+    print("No")
